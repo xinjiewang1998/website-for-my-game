487e086121eeee4975aa56b04528498909f83f33
u7201825@anu.edu.au <u7201825@anu.edu.au>
2023-02-22 19:50:25 +1100

    initial commit

---
+++ script/nav.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("main_container").style.width = "175px";
+  }
+  
+  function closeNav() {
+    document.getElementById("main_container").style.width = "0";
+  }
+  function changeStyle(){
+    document.getElementById("style").href = "./css/alt_main.css";
+  }
